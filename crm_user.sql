406950e24943793e1dde20b4b34dda7991b2f59c
TanjibQuad <tanjib@quadque.tech>
2023-12-21 15:19:57 +0600

    sql

--- crm_user.sql
+++ crm_user.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Dec 21, 2023 at 09:10 AM
+-- Generation Time: Dec 21, 2023 at 09:19 AM
 -- Server version: 10.5.19-MariaDB-cll-lve
 -- PHP Version: 7.2.34
 
