4369d9c6d484a609e08be8ab2210e9d79bdfcaca
TanjibQuad <tanjib@quadque.tech>
2023-12-28 12:05:42 +0600

    forgrt password

--- app/Http/Controllers/Api/ForgetPasswordController.php
+++ app/Http/Controllers/Api/ForgetPasswordController.php
@@ -62,7 +62,7 @@ class ForgetPasswordController extends Controller
 
         DB::table('password_resets')->where(['email' => $request->email])->delete();
 
-        return redirect('/login')->with('message', 'Your password has been changed!');
+        return view('auth.redirectToLogin');
     }
 
     public function redirectLogin()
--- routes/api.php
+++ routes/api.php
@@ -36,7 +36,7 @@ Route::post('/user/password-reset', [ForgetPasswordController::class, 'resetPass
 Route::post('/user/forgot-password', [ForgetPasswordController::class, 'forgotPassword']);
 Route::get('reset-password/{token}', [ForgetPasswordController::class, 'showResetPasswordForm'])->name('reset.password.get');
 Route::post('reset-password', [ForgetPasswordController::class, 'submitResetPasswordForm'])->name('reset.password.post');
-Route::post('redirect-login', [ForgetPasswordController::class, 'redirectLogin'])->name('redirect.login');
+Route::get('redirect-login', [ForgetPasswordController::class, 'redirectLogin'])->name('redirect.login');
 
 
 Route::post('/user/check-verification', [\App\Http\Controllers\Api\AuthController::class, 'updateVerificationCode']);
