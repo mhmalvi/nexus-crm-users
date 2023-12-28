cbc1e0e71fd1c54c06b013878b2af7c599918f6d
TanjibQuad <tanjib@quadque.tech>
2023-12-28 13:04:34 +0600

    forgrt password

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -644,7 +644,7 @@ class AuthController extends Controller
             $roleArray = [1, 2, 3, 4, 5];
             if (isset($data->role_id) && in_array($data->role_id, $roleArray)) {
                 // $clientId =  $data->user_id;
-                $companyServiceAPI = env('COMPANY_SERVICE_API');
+                $companyServiceAPI = env('COMPANY_SERVICE_API','');
                 dd($companyServiceAPI);
 
                 $response = Http::post($companyServiceAPI . '/company/details/user', [
