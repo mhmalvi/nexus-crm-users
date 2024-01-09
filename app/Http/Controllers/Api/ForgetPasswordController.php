d4fdbad3b0517901b51d19199721ae45e6694c1b
TanjibQuad <tanjib@quadque.tech>
2024-01-09 11:27:11 +0600

    password validation

--- app/Http/Controllers/Api/ForgetPasswordController.php
+++ app/Http/Controllers/Api/ForgetPasswordController.php
@@ -75,7 +75,7 @@ class ForgetPasswordController extends Controller
             DB::table('password_resets')->where(['email' => $request->email])->delete();
 
             return view('auth.redirectToLogin');
-        } else if ($validator->fails()) {
+        } else {
             // return response()->json([
             //     'message' => 'Email is not valid',
             //     'status' => 500,
