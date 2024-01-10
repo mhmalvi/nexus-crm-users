cc15f6925fb22ef1fe965eba3c8cf24d45c0548a
TanjibQuad <tanjib@quadque.tech>
2024-01-10 10:20:48 +0600

    password validation

--- resources/views/auth/forgetPassword.blade.php
+++ resources/views/auth/forgetPassword.blade.php
@@ -5,9 +5,9 @@
     <div class="cotainer">
         <div class="row justify-content-center">
             <div class="col-md-8">
-                @if ($errors->any())
+                @if ($error)
                 <div class="alert alert-danger" role="alert">
-                    {!! implode('', $errors->all('<div>:message</div>')) !!}
+                    {{ $error }}
                 </div>
                 @endif
 
