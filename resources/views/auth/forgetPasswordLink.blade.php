4f9ac822f6fdeb32b0d71e1f2abf0e0ec8429029
TanjibQuad <tanjib@quadque.tech>
2024-01-10 17:01:11 +0600

    password validation

--- resources/views/auth/forgetPasswordLink.blade.php
+++ resources/views/auth/forgetPasswordLink.blade.php
@@ -31,10 +31,11 @@
                             <label for="password" class="col-md-4 col-form-label text-md-right">Password</label>
                             <div class="col-md-6">
                                 <input type="password" id="password" class="form-control" name="password" required autofocus>
-                                @if (isset($errors))
-                                <div class="text-danger">{{ $errors->first('password') }}</div>
-                                @endif
                             </div>
+                            @if (isset($errors))
+                            <div class="text-danger">{{ $errors->first('password') }}</div>
+                            @endif
+
                         </div>
 
                         <div class="form-group row">
