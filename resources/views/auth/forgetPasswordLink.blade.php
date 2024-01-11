dfcac6758e1b0c1ee374b73cc4818fb977003ae1
TanjibQuad <tanjib@quadque.tech>
2024-01-11 10:25:00 +0600

    password validation

--- resources/views/auth/forgetPasswordLink.blade.php
+++ resources/views/auth/forgetPasswordLink.blade.php
@@ -8,13 +8,6 @@
                 <div class="card">
                     <div class="card-header">Reset Password</div>
                     <div class="card-body">
-                        {{-- @if ($errors->any())
-                        <div class="alert alert-danger" role="alert">
-                            @foreach($errors->all() as $error)
-                            <li>{{ $error }}</li>
-                        @endforeach
-                    </div>
-                    @endif --}}
                     <form action="{{ route('reset.password.post') }}" method="POST">
                         @csrf
                         <input type="hidden" name="token" value="{{ $token }}">
