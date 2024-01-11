0947f6e4417c591aec6bdabcd2d20b9de727befc
TanjibQuad <tanjib@quadque.tech>
2024-01-11 09:43:12 +0600

    password validation

--- app/Http/Controllers/Api/ForgetPasswordController.php
+++ app/Http/Controllers/Api/ForgetPasswordController.php
@@ -58,7 +58,7 @@ class ForgetPasswordController extends Controller
         ]);
         // dd(json_decode($validator->errors()));
         if ($validator->fails()) {
-            return back()->with('error', $validator->errors());
+            return back()->withInput()->with('error', $validator->errors());
             // return response()->json([
             //     'data' => $validator->errors()
             // ]);
