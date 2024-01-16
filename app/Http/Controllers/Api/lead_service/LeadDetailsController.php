e33ce59fb3223796223e2818fa267b3f08453708
TanjibQuad <tanjib@quadque.tech>
2024-01-16 14:28:44 +0600

    lead service api integrated

--- app/Http/Controllers/Api/lead_service/LeadDetailsController.php
+++ app/Http/Controllers/Api/lead_service/LeadDetailsController.php
@@ -8,8 +8,9 @@ use Illuminate\Support\Facades\Http;
 
 class LeadDetailsController extends Controller
 {
-    public function leadDetails(Request $request){
-        $lead_details = Http::post(env('LEAD_SERVICE_API').'/lead/details',['lead_id'=>$request->lead_id]);
+    public function leadDetails(Request $request)
+    {
+        $lead_details = Http::post(env('LEAD_SERVICE_API','') . '/lead/details', ['lead_id' => $request->lead_id]);
         return response()->json($lead_details);
     }
 }
