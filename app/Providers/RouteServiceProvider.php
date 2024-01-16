69aaeef44caa1523070f22fe59ebd9a3a013cecf
TanjibQuad <tanjib@quadque.tech>
2024-01-16 12:53:50 +0600

    lead service api integrated

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -43,7 +43,7 @@ class RouteServiceProvider extends ServiceProvider
                 ->namespace($this->namespace)
                 ->group(base_path('routes/api.php'));
 
-            Route::prefix('api')
+            Route::prefix('lead-api')
                 ->middleware('api')
                 ->namespace($this->namespace)
                 ->group(base_path('routes/lead_service_api.php'));
