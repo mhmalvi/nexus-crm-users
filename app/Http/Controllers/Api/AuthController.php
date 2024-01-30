a4dfe0d1a4e8b8847354bf38e32b39941f1ac835
TanjibQuad <tanjib@quadque.tech>
2024-01-30 12:20:48 +0600

    logout fix

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -712,7 +712,7 @@ class AuthController extends Controller
     public function logout(Request $request)
     {
          // dd(json_decode(auth('sanctum')->user()->currentAccessToken())->tokenable->token);
-         if (auth('sanctum')->user() !== null) {
+         if ($request->bearerToken() !== null) {
             $response = auth('sanctum')->user()->currentAccessToken()->delete();
             $token_exist = ActiveToken::where('token', $request->bearerToken())->where('ip', $request->ip())->exists();
             // dd($token);
