e939182b10cbcc1f55a36d3edabda223f4d9cbcc
TanjibQuad <tanjib@quadque.tech>
2024-02-11 10:20:29 +0600

    user exists

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -218,50 +218,21 @@ class AuthController extends Controller
      */
     public function createUser(Request $request)
     {
-        // dd($request->all());
         DB::beginTransaction();
 
         try {
-            //Validated
-            // if($request->role_id !== 5){
-            //     $validateUser = Validator::make($request->all(),
-            //         [
-            //             'email' => 'required|email|unique:users,email',
-            //             'role_id' => 'required'
-            //         ]);
-            //     if($validateUser->fails()){
-            //         return response()->json([
-            //             'status' => false,
-            //             'message' => 'validation error',
-            //             'errors' => $validateUser->errors()
-            //         ], 401);
-            //     }
-            // }
-            // $randomPassword = $this->_randomPassword();
-            // Business type used in Company service this time
-            // $userId = DB::table('users')->insertGetId([
-            //     'email' => $request->email,
-            //     // 'password' => Hash::make($randomPassword),
-            //     'role_id' => $request->role_id,
-            //     'contact_number' => isset($request->contact_number) ? $request->contact_number : '',
-            //     // 'abn_number' => isset($request->abn_number)?$request->abn_number:'',
-            //     'flag' => 1,
-            //     'status' => 1,
-            //     'suspend' => 0,
-            //     'created_at' => Carbon::parse(now())->toDateTime(),
-            //     'updated_at' => Carbon::parse(now())->toDateTime()
-            // ]);
+            
             $user = User::where('email', $request->email)->first();
             $user->contact_number = $request->contact;
             $user->verification_status = 2;
             $user->save();
-            // dd($user);
             $profile = UserProfile::where('user_id', $user->id)->first();
             $profile->full_name = $request->username;
             $profile->website = $request->website;
             $profile->address = $request->company_address;
             $profile->save();
-            DB::connection('company')->table('companies')->insert([
+
+            $company = DB::connection('company')->table('companies')->insert([
                 'name' => $request->company_name,
                 'contact' => $request->contact,
                 'business_email' => $request->email,
@@ -275,20 +246,7 @@ class AuthController extends Controller
                 'active' => 1,
                 'industry' => $request->industry
             ]);
-            // DB::table('user_profile')->insert([
-            //     'user_id' => $userId,
-            //     'full_name' => isset($request->full_name) ? $request->full_name : '',
-            //     'address' => isset($request->address) ? $request->address : '',
-            //     'qualification' => isset($request->qualification) ? $request->qualification : '',
-            //     'region' => isset($request->region) ? $request->region : '',
-            //     'postcode' => isset($request->postcode) ? $request->postcode : '',
-            //     'work_experiences' => isset($request->work_experiences) ? $request->work_experiences : '',
-            //     'location' => isset($request->location) ? $request->location : '',
-            //     'profession' => isset($request->profession) ? $request->profession : '',
-            //     'secondary_contact' => isset($request->secondary_contact) ? $request->secondary_contact : '',
-            //     'date_of_birth' => isset($request->date_of_birth) ? $request->date_of_birth : '',
-            //     'website' => isset($request->website) ? $request->website : ''
-            // ]);
+            dd($company);
             DB::commit();
 
             Mail::to($request->email)->queue(new RegistrationMail($request->email, $request->full_name));
