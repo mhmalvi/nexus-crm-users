5ef188c15631de6eb600e7b2b436b7eef7074e65
TanjibQuad <tanjib@quadque.tech>
2024-02-13 11:11:06 +0600

    bug fix

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -258,7 +258,7 @@ class AuthController extends Controller
             ///////////////////////////////////////////////////////////////////////////////////
             $file->client_id = $company->id;
             $file->save();
-            $user_data = DB::table('users')->join('user_profile', 'users.id' === 'user_profile.user_id')->where('users.id', $user->id)->first();
+            $user_data = DB::table('users')->join('user_profile', 'users.id', '=', 'user_profile.user_id')->where('users.id', $user->id)->first();
             Mail::to($request->email)->queue(new RegistrationMail($request->email, $request->full_name));
             DB::commit();
             return response()->json([
