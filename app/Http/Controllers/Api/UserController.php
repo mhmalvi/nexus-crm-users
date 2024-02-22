7bbc80864b0026dcbac02741d7e3c252dd567c18
TanjibQuad <tanjib@quadque.tech>
2024-02-22 08:55:32 +0600

    add user api created

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -12,6 +12,18 @@ use Illuminate\Support\Facades\Mail;
 
 class UserController extends Controller
 {
+    private function _randomPassword()
+    {
+        $alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890' . time();
+        $pass = array(); //remember to declare $pass as an array
+        $alphaLength = strlen($alphabet) - 1; //put the length -1 in cache
+        for ($i = 0; $i < 8; $i++) {
+            $n = rand(0, $alphaLength);
+            $pass[] = $alphabet[$n];
+        }
+        return implode($pass);
+        //turn the array into a string 
+    }
     public function addUser(Request $request)
     {
         DB::beginTransaction();
