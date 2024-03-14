b13a395989bf538da29f3d2fd924124ea7d03999
TanjibQuad <tanjib@quadque.tech>
2024-03-14 10:42:44 +0600

    login fix

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -736,7 +736,7 @@ class AuthController extends Controller
             } else {
                 return response()->json([
                     'message'=>'Account not verified',
-                    'status' => 222,
+                    'token'=>""
                 ]);
             }
         } catch (\Throwable $th) {
