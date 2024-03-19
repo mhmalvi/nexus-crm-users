b7aa8d29928f027ff604e8dd069b7d145ee4ed37
TanjibQuad <tanjib@quadque.tech>
2024-03-19 09:53:00 +0600

    bug fix

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -703,12 +703,17 @@ class AuthController extends Controller
                     if ($jsonArray != "" && isset($jsonArray->data->company_id)) {
                         $clientId = $jsonArray->data->company_id;
                         $ac_k = $jsonArray->data->fb_ac_credential;
-                        $customer_id = $jsonArray->data->connect_id?$jsonArray->data->connect_id:"";
+                        if($jsonArray->data->connect_id){
+                            $customer_id = $jsonArray->data->connect_id;
+                        }
+                        
                     }
                 }
                 $data->client_id = $clientId;
                 $data->ac_k = $ac_k;
-                $data->customer_id = $customer_id;
+                if($customer_id){
+                    $data->customer_id = $customer_id;
+                }                
                 $token = Auth::user()->createToken("API TOKEN")->plainTextToken;
                 // $user = User::where('email', $request->email)->first();
                 // // dd($user);
