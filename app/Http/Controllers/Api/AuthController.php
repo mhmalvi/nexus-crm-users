3593328ef9de139bcbcd8f2a3ea5da3118d74332
TanjibQuad <tanjib@quadque.tech>
2024-04-18 10:09:30 +0600

    latest

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -333,7 +333,7 @@ class AuthController extends Controller
             }
             $company->save();
             $user_data = DB::table('users')->join('user_profile', 'users.id', '=',
-            'user_profile.user_id')->leftJoin('companies','users.id', '=', 'companies.admin')->where('users.id',
+            'user_profile.user_id')->join('companies','users.id', '=', 'companies.admin')->where('users.id',
             $user->id)->first();
             Mail::to($request->email)->queue(new RegistrationMail($request->email, $request->full_name));
             // DB::commit();
